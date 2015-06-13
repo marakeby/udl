c63886d8f85d2c969da03c326f3bbd888fe9469b
haitham <marakeby@vt.edu>
2015-06-12 20:30:05 -0400

    remove hardcoded numbers. encapsulate data assignment to solver

--- udl/caffei/models/logistic.py
+++ udl/caffei/models/logistic.py
@@ -1,66 +1,48 @@
-import os
-
 import caffe
-from udl.caffei.models.layers.data_layers import DummyDataLayer, DummyDataParameter
-
-# from udl.caffei.models.layers.innerproduct import InnerProduct
-# from udl.caffei.models.layers.innerproduct import WeightFiller
-# from udl.caffei.models.layers.datalayer import DataLayer, DummyDataParams
-# from udl.caffei.models.layers.loss import SoftmaxLoss, Softmax
+from udl.caffei.datasets.DummyDataset import DummyDataset
 from udl.caffei.models.layers.innerproduct_pb2 import InnerProduct
 from udl.caffei.models.layers.layer import Phase
 from udl.caffei.models.layers.loss_layers import Loss, LossType
 from udl.caffei.models.layers.network import Network
+from udl.caffei.models.utils import get4D
 from udl.caffei.trainers.SGDTrainer import SGDTrainer
 from udl.model import UDLModel
-import numpy as np
 
 class Logistic(UDLModel):
-    def __init__(self, ):
+    def __init__(self,  dataset_adaptor=DummyDataset(), trainer=SGDTrainer()):
         caffe.set_mode_cpu()
-        dname = os.path.dirname(os.path.realpath(__file__))
-        # self.net_file= os.path.join(dname, 'train_in_memory.prototxt')
-        self.net_file = self.get_net()
-        # f = self.get_params_file ()
-        # self.solver = caffe.SGDSolver(f)
+        self.dataset_adaptor = dataset_adaptor
+        self.trainer = trainer
+
 
     def fit(self, X, Y):
-        model = self.get_net()
-        self.solver = SGDTrainer().get_trainer(model, X, Y)
+        X4D = get4D(X)
+        Y4D = get4D(Y)
+        data_layer = self.dataset_adaptor.fit_transform(X4D,Y4D)
+        net = self.get_net(data_layer)
+        self.solver = self.trainer.get_trainer(net)
+        self.solver.set_train_data (X4D, Y4D)
         self.solver.solve()
         return self.solver
+        
 
     def predict(self, X):
-        [a, b] = X.shape
-        data4D = X.reshape((a, b, 1, 1))
-        # Y4D= Yt.reshape((a, 1, 1,1))
-        sh = data4D.shape
-        print data4D.shape
-        self.solver.test_nets[0].blobs['data'].reshape (sh[0], sh[1], sh[2], sh[3])
-        self.solver.test_nets[0].blobs['data'].data[...] = data4D
-
-        # self.solver.test_nets[0].set_input_arrays(data4D, np.random.randint(10,  size=self.solver.test_nets[0].blobs['label'].data.shape))
+        X4D = get4D(X)
+        self.solver.set_test_data (X4D)
         self.solver.test_nets[0].forward()
-
-
-
         prob = self.solver.test_nets[0].blobs['output'].data
         print prob.shape
         pred = prob[:, 0] < prob[:, 1]
         return pred
 
-    def get_net(self):
+    def get_net(self, data_layer):
         n = Network()
+        n.add(data_layer)
         n.name = "LogisticRegressionNet"
-        d1 = DummyDataLayer(name='data', top=['data', 'label'],
-                            params=DummyDataParameter([7500, 7500], [4, 1], [1, 1], [1, 1]))
-        # d2 = DummyDataLayer(name='data', top=['data', 'label'], include=Phase.TEST,params = DummyDataParameter([2500,2500],[4,1],[1,1],[1,1]))
         l = InnerProduct(name="fc1", top="fc1", bottom="data", num_output=2)
         l3 = Loss(name="loss", type=LossType.SoftmaxWithLoss, bottom=["fc1", "label"], include=Phase.TRAIN)
         l4 = Loss(name="output", type= LossType.SOFTMAX, top= "output", bottom="fc1", include=Phase.TEST)
 
-        n.add(d1)
-        # n.add(d2)
         n.add(l)
         n.add(l3)
         n.add(l4)
