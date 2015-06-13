c8620f364a95772dab89c5bb44f41a66015c60c4
haitham <marakeby@vt.edu>
2015-06-12 20:31:50 -0400

    remove hardcoded numbers. encapsulate data assignment to solver

---
+++ udl/caffei/models/utils.py
@@ -0,0 +1,12 @@
+
+
+__author__ = 'haitham'
+import numpy as np
+def get4D( X):
+        sh  = np.asarray(X.shape)
+        sh.resize((1,4))
+        sh = sh[0]
+        sh[sh==0]=1
+        X = X.reshape(sh[0], sh[1], sh[2], sh[3])
+        return X
+
